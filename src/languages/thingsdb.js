992dcaad521a7ba04b07e1f45c98d9dd397f85bb
Jeroen van der Heijden <jeroen@transceptor.technology>
2019-12-24 13:52:31 +0100

    Update thingsdb language

--- src/languages/thingsdb.js
+++ src/languages/thingsdb.js
@@ -59,22 +59,39 @@ function(hljs) {
                 beginKeywords: 'true false nil'
             },
             {
-                className: 'build_in',
-                beginKeywords: 'assert assert_err auth_err backup_info backups_info ' +
-                'bad_data_err bool closure collection_info collections_info ' +
-                'counters deep del_backup del_collection del_expired del_node ' +
-                'del_procedure del_token del_type del_user err float ' +
-                'forbidden_err grant int isarray isascii isbool isbytes iserr ' +
-                'isfloat isinf isint islist isnan isnil israw isset isstr ' +
-                'isthing istuple isutf8 list lookup_err max_quota_err mod_type new ' +
-                'new_backup new_collection new_node new_procedure new_token ' +
-                'new_type new_user node_err node_info nodes_info now ' +
-                'num_arguments_err operation_err overflow_err procedure_doc ' +
-                'procedure_info procedures_info raise refs rename_collection ' +
-                'rename_user reset_counters return revoke run set_log_level set ' +
-                'set_quota set_type shutdown str syntax_err thing try type ' +
-                'type_err type_count type_info types_info user_info users_info ' +
-                'value_err wse zero_div_err',
+                className: 'function',
+                begin: new RegExp(
+                    '\\b(' +
+                    /* collection functions */
+                    'assert|base64_encode|base64_decode|bool|bytes|' +
+                    'deep|del_type|err|float|has_type|if|int|isarray|' +
+                    'isascii|isbool|isbytes|iserr|isfloat|isinf|isint|' +
+                    'islist|isnan|isnil|israw|isset|isstr|isthing|istuple|' +
+                    'isutf8|list|mod_type|new|new_type|now|raise|rand|' +
+                    'randint|refs|return|set|set_type|str|thing|try|type|' +
+                    'type_count|type_info|types_info|wse|' +
+                    /* node functions */
+                    'backup_info|backups_info|counters|del_backup|' +
+                    'has_backup|new_backup|node_info|nodes_info|' +
+                    'reset_counters|set_log_level|shutdown|' +
+                    /* thingsdb functions */
+                    'collection_info|collections_info|del_collection|' +
+                    'del_expired|del_node|del_token|del_user|grant|' +
+                    'has_collection|has_node|has_token|has_user|' +
+                    'new_collection|new_node|new_token|new_user|' +
+                    'rename_collection|rename_user|revoke|set_password|' +
+                    'user_info|users_info|' +
+                    /* procedure functions */
+                    'del_procedure|has_procedure|new_procedure|' +
+                    'procedure_doc|procedure_info|procedures_info|run|' +
+                    /* error functions */
+                    'assert_err|auth_err|bad_data_err|forbidden_err|' +
+                    'lookup_err|max_quota_err|node_err|num_arguments_err|' +
+                    'operation_err|overflow_err|syntax_err|type_err|' +
+                    'value_err|zero_div_err' +
+                    /* end */
+                    ')\\s*(?=\\()'
+                )
             },
             {
                 className: 'function',
@@ -82,7 +99,8 @@ function(hljs) {
                     '\\.\\s*(len|call|doc|code|msg|extend|filter|find|' +
                     'findindex|indexof|map|pop|push|remove|sort|splice|' +
                     'add|has|contains|endswith|lower|startswith|test|upper|' +
-                    'del|get|id|keys|set|values|wrap|unwrap)\\s*(?=\\()'),
+                    'del|get|id|keys|set|values|wrap|unwrap)\\s*(?=\\()'
+                ),
             },
             {
                 className: 'attr',
