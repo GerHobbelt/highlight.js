498980468b6fec85fc0a649a2e1de11d665890ab
Jeroen van der Heijden <jeroen@transceptor.technology>
2020-02-19 23:08:05 +0100

    Update with choice, watch, unwatch, every and some functions

--- src/languages/thingsdb.js
+++ src/languages/thingsdb.js
@@ -96,10 +96,10 @@ function(hljs) {
             {
                 className: 'function',
                 begin: new RegExp(
-                    '\\.\\s*(len|call|doc|code|msg|extend|filter|find|' +
+                    '\\.\\s*(len|call|choice|doc|code|msg|every|extend|filter|find|' +
                     'findindex|indexof|map|pop|push|remove|sort|splice|' +
                     'add|has|contains|endswith|lower|startswith|test|upper|' +
-                    'del|get|id|keys|reduce|set|values|wrap|unwrap)\\s*(?=\\()'
+                    'del|get|id|keys|reduce|set|some|values|watch|wrap|unwatch|unwrap)\\s*(?=\\()'
                 ),
             },
             {
